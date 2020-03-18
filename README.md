738359bae8b926b21fb896da107b77e5774d004e
Lucas Werner <lucaswerner26@gmail.com>
2020-03-18 01:40:47 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# my-card-config-server" 
---
+++ src/test/java/com/mycard/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycard.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
